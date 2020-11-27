6590e29c130934082e4550f0316e7237818f0c96
SalsaCoentros <73700763+SalsaCoentros@users.noreply.github.com>
2020-11-27 12:32:21 +0000

    Lab3_Kp_Ki

--- lab3/main.m
+++ lab3/main.m
@@ -17,6 +17,22 @@ B = arx241.B;
 fuzzyInf = readfis('pifuz.fis'); % Fuzzy Structure
 
 r = [2*ones(150,1);4*ones(150,1);3*ones(150,1); 4.5*ones(150,1);3*ones(150,1)]; 
+%r = [ones(750,1)];
+
+M = 3;
+Ts = 0.08;
+
+%regra ziegler nichols
+T = 1.5;
+L = 0.5;
+
+Kp = 0.9*(T/L);
+Ki = L/0.3;
+
+Ti = Kp/Ki;
+
+Ku = (Kp*M*Ts)/Ti
+
 N = length(r);
 Ts = 0.08; % Intervalo de amostragem 80 ms
 
@@ -24,11 +40,11 @@ Ts = 0.08; % Intervalo de amostragem 80 ms
 ubound = [0 5];
 error = 0;
 derror = 0;
-Kerror = 0.75;
-Kderror = .5;
+Kerror = 0.8*1/M;
+Kderror = 1.2*Ti/M;
 a = 0.015;
-Kdu = a*diff(ubound);
-%Kdu = 0.05;
+% Kdu = a*diff(ubound);
+Kdu = 0.5*Ku;
 % *************************************************
 
 u = zeros(N,1);
@@ -42,7 +58,7 @@ for index = 5 : N
         derror = r(index)-r(index-1)-y(index)+y(index-1);
         u(index,1)=pifuzzy(fuzzyInf, error, derror, Kerror, Kderror, Kdu, u(index-1,1), ubound);
     end
-    y(index+1) = -A(2)*y(index,1) - A(3)*y(index-1,1) + B(2)*u(index-1,1) + B(3)*u(index-2,1) + B(4)*u(index-3,1) + + B(5)*u(index-4,1) ;
+    y(index+1) = -A(2)*y(index,1) - A(3)*y(index-1,1) + B(2)*u(index-1,1) + B(3)*u(index-2,1) + B(4)*u(index-3,1) + + B(5)*u(index-4,1) + rand(1)*0.05 ;
     %y(index+1) = -theta(1)*y(index,1)-theta(2)*y(index-1,1)-theta(3)*y(index-2,1)+...
     %theta(4)*u(index,1)+theta(4)*u(index-1,1)+0.05*randn;
   
